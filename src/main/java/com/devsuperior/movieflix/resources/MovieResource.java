bff32cbc88bec12c5747df5c3e83ebf247898811
MarcoJunior <mcarrarajr@uol.com.br>
2023-01-20 11:14:10 -0300

    Inclusão Endpoints

---
+++ src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
@@ -0,0 +1,45 @@
+package com.devsuperior.movieflix.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.services.MovieService;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @Autowired
+    private MovieService service;
+
+    @GetMapping
+    public ResponseEntity<Page<MovieDTO>> findByGenre(
+            @RequestParam(defaultValue = "0") Long genreId,
+            @PageableDefault(sort = { "title" }) Pageable pageable
+    ) {
+        Page<MovieDTO> result = service.findAll(genreId, pageable);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping("/{id}/reviews")
+    public ResponseEntity<List<ReviewDTO>> findMovieReviews(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findMovieReviews(id));
+    }
+}
